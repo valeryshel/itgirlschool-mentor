4af975a1da5c8bd247616a45d3245b89d276e0f9
valeryshel <valeryshel@gmail.com>
2023-03-05 17:55:42 +0300

    add new js files

--- js/lesson14.js
+++ js/lesson14.js
@@ -13,60 +13,80 @@ let object = {
   age: 30,
 };
 
+//! переменные
 var a = "var"; //устаревшее
 let b = "let";
-const C = "const";
+const RED = "red";
 
-//! hoisting - поднятие
-console.log(d); // var поднимается
 var d; // var объявлена, но не назначена
-// console.log(e); // let не поднимается
+console.log(d);
 let e;
+console.log(e);
 // let e = 20; // повторное объявление let вызовет ошибку, нужно переназначить переменную
 // e = 20;
-// console.log(F); const не поднимается
-const F = 30;
-// const F; // const без назначения выдает ошибку
+
+// console.log(INTEGER);
+// const INTEGER; // const без назначения выдает ошибку
+//const INTEGER = 30;
 var d = 10; // var можно объявить и назначить повторно
 
-//! область видимости
-// console.log(aaa); // ошибка
-{
-  //console.log(aaa); // 123
-}
-// console.log(aaa); // ошибка
-
-console.log(bbb); // undefined
-{
-  var bbb = 234;
-  // console.log(bbb); //234
-}
-// console.log(bbb); // 234
-
-if (bbb == 234) {
-  var ccc = 111;
-  // console.log(ccc); //111
-}
-// console.log(ccc); //111
-
-let array = [1, 2, 3];
-for (var index = 0; index < array.length; index++) {
-  console.log(index);
-}
-console.log(index);
-// console.log(element);
-
-// создаем функцию с var
-let varFunction = function () {
-  var qqq = 444;
-  console.log(qqq);
-};
-varFunction(); // вызываем функцию
-//console.log(qqq); // выдаст ошибку, т.к. var внутри функции
-
-//! Конкатенация
+//! Конкатенация и интерполяция
 console.log("Hello" + " " + "World!");
 console.log(5 + "55");
 console.log(5 + 55);
 console.log(5 + "hello world!");
 console.log(`hello world ${bbb + ccc}`); // шаблонные литералы - интерполяция
+
+//! логические операторы
+//* ИЛИ "||" находит первое истинное значение
+console.log(true || true); // true
+console.log(false || true); // true
+console.log(true || false); // true
+console.log(false || false); // false
+console.log(1 || 0); // 1
+console.log(true || "no matter what"); // true
+console.log(null || 1); // 1 (первое истинное значение)
+console.log(null || 0 || 1); // 1 (первое истинное значение)
+console.log(undefined || null || 0); // 0 (поскольку все ложно, возвращается последнее значение)
+
+//* И «&&» находит первое ложное значение
+console.log(true && true); // true
+console.log(false && true); // false
+console.log(true && false); // false
+console.log(false && false); // false
+// Если первый операнд истинный,
+// И возвращает второй:
+console.log(1 && 0); // 0
+console.log(1 && 5); // 5
+
+// Если первый операнд ложный,
+// И возвращает его. Второй операнд игнорируется
+console.log(null && 5); // null
+console.log(0 && "no matter what"); // 0
+// Можно передать несколько значений подряд. В таком случае возвратится первое «ложное» значение, на котором остановились вычисления.
+
+console.log(1 && 2 && null && 3); // null
+// Когда все значения верны, возвращается последнее
+console.log(1 && 2 && 3); // 3
+
+//?Приоритет оператора && больше, чем у || так что он выполняется раньше.
+
+//* a && b || c && d = (a && b) || (c && d);
+
+//! !(НЕ) Оператор принимает один аргумент и выполняет следующие действия
+//Сначала приводит аргумент к логическому типу true/false.
+// Затем возвращает противоположное значение.
+console.log(!true); // false
+console.log(!0); // true
+//? двойное НЕ !! используют для преобразования значений к логическому типу
+console.log(!!"non-empty string"); // true
+console.log(!!null); // false
+
+//? Приоритет НЕ ! является наивысшим из всех логических операторов, поэтому он всегда выполняется первым, перед && или ||.
+
+//* PRACTICE
+console.log(null || 2 || undefined);
+alert(alert(1) || 2 || alert(3));
+alert(1 && null && 2);
+alert(alert(1) && alert(2));
+alert(null || (2 && 3) || 4);
---
+++ js/lesson15.js
@@ -0,0 +1,149 @@
+console.log("hello again JavaScript!");
+
+//! область видимости
+// console.log(aaa); // ошибка
+{
+  //console.log(aaa); // 123
+}
+// console.log(aaa); // ошибка
+
+console.log(bbb); // undefined
+{
+  var bbb = 234;
+  // console.log(bbb); //234
+}
+// console.log(bbb); // 234
+
+if (bbb == 234) {
+  var ccc = 111;
+  // console.log(ccc); //111
+}
+// console.log(ccc); //111
+
+let array = [1, 2, 3];
+for (var index = 0; index < array.length; index++) {
+  console.log(index);
+}
+console.log(index);
+// console.log(element);
+
+// создаем функцию с var
+let varFunction = function () {
+  var qqq = 444;
+  console.log(qqq);
+};
+varFunction(); // вызываем функцию
+//console.log(qqq); // выдаст ошибку, т.к. var внутри функции
+
+//! hoisting - поднятие переменных
+console.log(d); // var поднимается
+var d; // var объявлена, но не назначена
+// console.log(e); // let не поднимается
+let e;
+// let e = 20; // повторное объявление let вызовет ошибку, нужно переназначить переменную
+// e = 20;
+// console.log(F); const не поднимается
+const F = 30;
+// const F; // const без назначения выдает ошибку
+var d = 10; // var можно объявить и назначить повторно
+
+//!Поднятие функций
+//fuction declaration
+add(3, 4); // 7
+
+function add(a, b) {
+  return a + b;
+}
+
+// Функции function expression при таких условиях работать не будут:
+
+//multiply(3, 4); // TypeError
+
+var multiply = function (a, b) {
+  return a * b;
+};
+
+// Значение объявленной через var переменной переменной или function expression до того как интерпретатор дойдёт до её кода, равно undefined.
+
+// Рассмотрим пример:
+// var myname = "Anna";
+// function fn() {
+//   console.log(myname);
+//   var myname = "Sveta";
+//   console.log(myname);
+// }
+// console.log(myname);
+// fn();
+//? что выведет консоль и в каком порядке?
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+//.
+
+// undefined
+// Sveta
+// Anna
